0b154f9bd5d4cf1a58f1b1789fd9a2c257b1b74a
Pauskar <pauskar@iazi.ch>
2018-03-06 12:06:06 +0530

    appconfig added for different env configuration

---
+++ src/app/appconfig/config.int.ts
@@ -0,0 +1,4 @@
+export class Config{
+    public static readonly LAGECHECKASSETPATH: string = 'http://intservices.iazi.ch/api/lagecheckdoc/';
+    public static readonly LAGECHECKAPI: string = 'https://intservices.iazi.ch/api/LageCheck/';
+}
---
+++ src/app/appconfig/config.prod.ts
@@ -0,0 +1,4 @@
+export class Config{
+    public static readonly LAGECHECKASSETPATH: string = 'http://vmseiisdev01/api/lagecheckdoc/';
+    public static readonly LAGECHECKAPI: string = 'https://devapi.iazi.ch/api/LageCheck/';
+}
