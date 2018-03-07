d510fbd02feccf02a16c1ab69ee16c6c31e7bff1
Pauskar <pauskar@iazi.ch>
2018-03-07 11:48:40 +0530

    local config setting changes

--- src/app/appconfig/config.local.ts
+++ src/app/appconfig/config.local.ts
@@ -1,4 +1,4 @@
 export class Config{
-    public static readonly LAGECHECKASSETPATH: string = 'http://vmseiisdev01/api/lagecheckdoc/';
+    public static readonly LAGECHECKASSETPATH: string = 'http://localhost:4200/';
     public static readonly APPMAPSAPI: string = 'https://devapi.iazi.ch/api/appMaps/';
 }
