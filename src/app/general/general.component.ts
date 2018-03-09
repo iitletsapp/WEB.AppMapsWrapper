d3e3e9950f25081dd5785db1b294623958602890
Pauskar <pauskar@iazi.ch>
2018-03-09 17:04:49 +0530

    added kantons and muncipality images general info component updated

--- src/app/general/general.component.ts
+++ src/app/general/general.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit, Input, SimpleChanges } from '@angular/core';
 import { GetMarkerService } from '../../services/getmarker.service';
 import { GetMunicipalityService } from '../../services/getmunicipality.service';
-import { Globals} from '../globals'
+import { Globals} from '../globals';
+import { MacroService } from '../../services/macro.service';
 
 @Component({
     selector: 'app-general',
@@ -12,12 +13,17 @@ import { Globals} from '../globals'
 export class GeneralComponent implements OnInit {
     // public response: any;
     public generaldata;
+    public muncipalityId;
+    public cantonCode;
 
     constructor(
         private municipality: GetMunicipalityService,
-        public global:Globals
+        public global: Globals,
+        public macro: MacroService
     ) {
         this.generaldata = this.municipality.requestData('general');
+        this.muncipalityId = this.macro.macroObj.municipalityID;
+        this.cantonCode = this.generaldata[0].cantonCode;
     }
 
     public ngOnInit() { 
