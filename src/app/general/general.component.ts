e8ad16a8467fb5f5685cd4a4587e66688264da2f
Mendonca Lio <mendonca@iazi.ch>
2018-03-09 17:17:12 +0100

    refactor general component and add helper classes

--- src/app/general/general.component.ts
+++ src/app/general/general.component.ts
@@ -3,6 +3,7 @@ import { GetMarkerService } from '../../services/getmarker.service';
 import { GetMunicipalityService } from '../../services/getmunicipality.service';
 import { Globals} from '../globals';
 import { MacroService } from '../../services/macro.service';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
     selector: 'app-general',
@@ -15,18 +16,24 @@ export class GeneralComponent implements OnInit {
     public generaldata;
     public muncipalityId;
     public cantonCode;
+    public backgroundImg;
 
     constructor(
         private municipality: GetMunicipalityService,
         public global: Globals,
-        public macro: MacroService
+        public macro: MacroService,
+        private sanitizer: DomSanitizer
     ) {
         this.generaldata = this.municipality.requestData('general');
         this.muncipalityId = this.macro.macroObj.municipalityID;
         this.cantonCode = this.generaldata[0].cantonCode;
+        this.backgroundImg = sanitizer.bypassSecurityTrustStyle(
+            `url(${this.global.lageCheckAssetPath}/assets/img/kantons/${this.cantonCode}.jpg)`
+        );
+
     }
 
-    public ngOnInit() { 
+    public ngOnInit() {
     }
 
 
