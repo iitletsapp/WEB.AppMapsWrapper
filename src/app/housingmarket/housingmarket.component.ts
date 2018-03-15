d4ca5f9278d071acaa77aae070ed74451bb681d9
Anagha Chodankar <chodankar@iazi.ch>
2018-03-15 17:58:56 +0530

    Added polygons for tax and housing market components

--- src/app/housingmarket/housingmarket.component.ts
+++ src/app/housingmarket/housingmarket.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { MapService } from '../../services/map.service';
 import { MacroService } from '../../services/macro.service';
 import { GetMunicipalityService } from '../../services/getmunicipality.service';
+import { PolygonsService } from '../../services/polygons.service';
 
 @Component({
     selector: 'app-housingmarket',
@@ -11,25 +12,60 @@ import { GetMunicipalityService } from '../../services/getmunicipality.service';
 
 export class HousingMarketComponent implements OnInit {
     public housingMarket;
-    public dataforlinearrayMarket;  
+    public dataforlinearrayMarket;
     public housingMarketConstruction = [];
     public housingStock;
     public newHousing;
     public housingMarketVacancy;
     public xLabel = 'cost';
     public linecontainer = 'housingmarketchartcontainer';
+    public geoJson = '';
+    public extentData = [];
+    public extent = [];
 
-    constructor(private municipality: GetMunicipalityService) {
+    constructor(private municipality: GetMunicipalityService
+        , private mapService: MapService
+        , private polygonsService: PolygonsService
+    ) {
         this.housingMarket = this.municipality.requestData('housingMarket');
         this.housingMarketConstruction = this.municipality.requestData('housingMarketConstructionActivity');
         this.housingMarketVacancy = this.municipality.requestData('housingMarketVacancy');
-     }
+    }
 
-    public ngOnInit() { 
-      this.dataforlinearrayMarket = this.housingMarket.map((el) => { 
-      return { year: el.publishDate.toString().substring(0, 4), value: Math.floor(el.municipalityValue) };
-    }); 
-    this.housingStock = this.housingMarketConstruction[0].municipalityValue;
-    this.newHousing = this.housingMarketConstruction[1].municipalityValue; 
+    public ngOnInit() {
+        this.dataforlinearrayMarket = this.housingMarket.map((el) => {
+            return { year: el.publishDate.toString().substring(0, 4), value: Math.floor(el.municipalityValue) };
+        });
+        this.housingStock = this.housingMarketConstruction[0].municipalityValue;
+        this.newHousing = this.housingMarketConstruction[1].municipalityValue;
+
+        this.displayPolygons();
+    }
+
+    public displayPolygons() {
+        this.mapService.map.data.forEach((feature) => {
+            this.mapService.map.data.remove(feature);
+        });
+        this.geoJson = this.municipality.requestData('polygons');
+        this.mapService.map.data.addGeoJson(this.geoJson);
+        this.mapService.map.data.setMap(this.mapService.map);
+
+        this.polygonsService.zoom(this.mapService.map);
+
+        this.mapService.map.data.forEach((feature) => {
+            this.extentData.push(feature.f.privateRealEstatePriceIndexValue);
+        });
+
+        this.extent.push(Math.min.apply(null, this.extentData));
+        this.extent.push(Math.max.apply(null, this.extentData));
+
+        this.mapService.map.data.setStyle((feature) => {
+            const priceMarket = feature.f.privateRealEstatePriceIndexValue;
+            return {
+                fillColor: this.polygonsService.calcColor(priceMarket, this.extent),
+                strokeWeight: '2px',
+                strokeColor: this.polygonsService.calcColor(priceMarket, this.extent)
+            };
+        });
     }
 }
--- src/app/taxes/taxes.component.ts
+++ src/app/taxes/taxes.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { BarChartComponent } from '../charts/bar-chart/barchart.component';
 import { GetMunicipalityService } from '../../services/getmunicipality.service';
+import { MapService } from '../../services/map.service';
+import { PolygonsService } from '../../services/polygons.service';
 
 @Component({
   selector: 'app-taxes',
@@ -9,22 +11,57 @@ import { GetMunicipalityService } from '../../services/getmunicipality.service';
 })
 
 export class TaxesComponent implements OnInit {
- public tax;
- public dataforbarchart;
- public barchartcontainer = 'taxesbarchart';
+  public tax;
+  public dataforbarchart;
+  public barchartcontainer = 'taxesbarchart';
+  public geoJson = '';
+  public extentData = [];
+  public extent = [];
 
   public barchart: string = 'barchart';
   @ViewChild('BarChartComponent') bar: BarChartComponent;
-  constructor(  private municipality: GetMunicipalityService ) 
-      {
-        this.tax = this.municipality.requestData('tax');
-      }
+
+  constructor(private municipality: GetMunicipalityService
+    , private mapService: MapService
+    , private polygonsService: PolygonsService
+  ) {
+    this.tax = this.municipality.requestData('tax');
+  }
 
   public ngOnInit() {
-    this.dataforbarchart = this.tax.map((el) => { 
+    this.dataforbarchart = this.tax.map((el) => {
       return { name: el.what, value: Math.floor(el.municipalityValue) };
     });
-   }
+
+    this.displayPolygons();
+  }
+
+  public displayPolygons() {
+    this.mapService.map.data.forEach((feature) => {
+      this.mapService.map.data.remove(feature);
+    });
+    this.geoJson = this.municipality.requestData('polygons');
+    this.mapService.map.data.addGeoJson(this.geoJson);
+    this.mapService.map.data.setMap(this.mapService.map);
+
+    this.polygonsService.zoom(this.mapService.map);
+
+    this.mapService.map.data.forEach((feature) => {
+      this.extentData.push(feature.f.taxableIncomePerCapita);
+    });
+
+    this.extent.push(Math.min.apply(null, this.extentData));
+    this.extent.push(Math.max.apply(null, this.extentData));
+
+    this.mapService.map.data.setStyle((feature) => {
+      const taxCollections = feature.f.taxableIncomePerCapita;
+      return {
+        fillColor: this.polygonsService.calcColor(taxCollections, this.extent),
+        strokeWeight: '2px',
+        strokeColor: this.polygonsService.calcColor(taxCollections, this.extent)
+      };
+    });
+  }
 
 }
 
